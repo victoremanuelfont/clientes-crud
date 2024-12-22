3ecb59a46d1fec172ef7e6c7fd67a5a09098305c
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-22 11:44:58 -0300

    Criação do ClientDTO

--- src/main/java/com/crud/clientes/repositories/ClientRepository.java
+++ src/main/java/com/crud/clientes/repositories/ClientRepository.java
@@ -4,5 +4,5 @@ import com.crud.clientes.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    
+
 }
