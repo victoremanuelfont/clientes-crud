9fecab606895e8b77374b1c1c6a49757aef04efe
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-22 13:02:35 -0300

    Anotations necessárias para usar o postman na classe Controller e no métdodo findById()

--- src/main/java/com/crud/clientes/controller/ClientController.java
+++ src/main/java/com/crud/clientes/controller/ClientController.java
@@ -4,15 +4,20 @@ import com.crud.clientes.dto.ClientDTO;
 import com.crud.clientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/clients")
 public class ClientController {
 
     @Autowired
     private ClientService service;
 
-    public ResponseEntity<ClientDTO> findById(Long id){
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
