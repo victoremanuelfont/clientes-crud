f657e6fc68e6f25b852ffb87337bd34576d8b954
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-23 16:57:51 -0300

    ResourceNotFoundExceptions na camada de Serviço - service

---
+++ src/main/java/com/crud/clientes/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.crud.clientes.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+
+}
